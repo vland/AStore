5b2f27f80355f15fdefc764e82412e71ba66d390
vland <vland1990@gmail.com>
2015-02-07 19:16:03 +0200

    Added first view page

--- AStore/AStore/Controllers/HomeController.cs
+++ AStore/AStore/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace AStore.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello world";
+            return View();
         }
     }
 }
