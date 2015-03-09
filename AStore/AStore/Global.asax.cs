976553c3384dc7b223c72df559f618fb01b831d4
lezzi <lezzi1994@gmail.com>
2015-03-09 19:18:31 +0200

    Add localization

--- AStore/AStore/Global.asax.cs
+++ AStore/AStore/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +14,15 @@ namespace AStore
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            //Create culture info object 
+            CultureInfo ci = new CultureInfo("ru");
+
+            System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
+            System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
         }
     }
 }
